b10c61133f4204de73fcd243fe5ddb15fca2d6ea
NeptuneReal <38521285+Neptuneee11@users.noreply.github.com>
2023-12-04 07:45:37 +0800

    urls of train_maintenance done

--- CSCI41ProjectWHEEEEE/CSCI41ProjectWHEEEEE/urls.py
+++ CSCI41ProjectWHEEEEE/CSCI41ProjectWHEEEEE/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('train_maintenance/', include('train_maintenance.urls', namespace="train_maintenance")),
     path('admin/', admin.site.urls),
 ]
