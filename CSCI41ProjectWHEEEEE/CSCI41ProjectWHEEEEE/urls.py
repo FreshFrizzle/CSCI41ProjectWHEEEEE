0d3124abd40c4835dcd18bc3f2b557630ea76ffb
NeptuneReal <38521285+Neptuneee11@users.noreply.github.com>
2023-12-03 23:11:35 +0800

    urls.py stuff sorted out for train_operator

--- CSCI41ProjectWHEEEEE/CSCI41ProjectWHEEEEE/urls.py
+++ CSCI41ProjectWHEEEEE/CSCI41ProjectWHEEEEE/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('train_operator/', include('train_operator.urls', namespace="train_operator")),
     path('admin/', admin.site.urls),
 ]
