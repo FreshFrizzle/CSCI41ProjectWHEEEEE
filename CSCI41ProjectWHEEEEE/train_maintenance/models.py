781047f266673e0ecfcd7d7ec1c2e808ee875c61
NeptuneReal <38521285+Neptuneee11@users.noreply.github.com>
2023-12-04 08:27:39 +0800

    models.py classes done on train_maintencance

--- CSCI41ProjectWHEEEEE/train_maintenance/models.py
+++ CSCI41ProjectWHEEEEE/train_maintenance/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class Maintenance(models.Model):
+    Maintenance_Num = models.AutoField(primary_key=True, max_length=5)
+
+
+class Maintenance_Logs(models.Model):
+    Maintenance_Num = models.ForeignKey(
+        Maintenance,
+        on_delete=models.CASCADE,
+    )
+    Train_ID = models.OneToOneField(
+        Train,
+        on_delete=models.CASCADE,
+    )
+
+class Maintenance_Task(models.Model):
+    Maintenance_Num = models.ForeignKey(
+        Maintenance,
+        on_delete=models.CASCADE,
+    )
+
+    Task_ID = models.CharField(primary_key=True, max_length=5, default="00001")  
+
+class Task(models.Model):
+    Task_ID = models.ForeignKey(
+        Maintenance_Task,
+        on_delete=models.CASCADE,
+    )
+    Task_Name = models.CharField(primary_key=False, max_length=25, default="inspection")
+    Crew_In_Charge = models.CharField(primary_key=False, max_length=25, default="J. Doe")
+    Condition = models.CharField(primary_key=False, max_length=25, default="Horrible")
+
+
+
+
+
