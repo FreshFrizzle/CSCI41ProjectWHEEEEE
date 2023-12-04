80407ab11d465685b68365263f060e1278b09c18
NeptuneReal <38521285+Neptuneee11@users.noreply.github.com>
2023-12-04 21:50:20 +0800

    empty 3nf of train_maintenance

--- CSCI41ProjectWHEEEEE/train_maintenance/models.py
+++ CSCI41ProjectWHEEEEE/train_maintenance/models.py
@@ -1,32 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class TRAIN(models.Model):
-    Train_ID = models.CharField(primary_key=True, max_length=6, default="000001")
-    Model = models.OneToOneField(
-        
-    )
-
-class TRAIN_MODEL(models.Model):
+class MAINTENANCE(models.Model):
     pass
 
-class ROUTE(models.Model):
+class MAINTENANCE_TASK(models.Model):
     pass
 
-class S_SERIES(models.Model):
+class MAINTENANCE_LOGS(models.Model):
     pass
 
-class A_SERIES(models.Model):
+class TASK(models.Model):
     pass
 
-class LOCAL(models.Model):
-    pass
-
-class INTER_TOWN(models.Model):
-    pass
-
-class S_SERIES_ROUTING(models.Model):
-    pass
-
-class A_SERIES_ROUTING(models.Model):
-    pass
