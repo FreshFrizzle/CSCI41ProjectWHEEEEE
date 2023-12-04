9f2f5f58837d2ba11a3eff614f77a22bfcb602ed
NeptuneReal <38521285+Neptuneee11@users.noreply.github.com>
2023-12-04 21:46:15 +0800

    empty enf of trainOperator

--- CSCI41ProjectWHEEEEE/train_operator/models.py
+++ CSCI41ProjectWHEEEEE/train_operator/models.py
@@ -4,52 +4,35 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
-class Train(models.Model):
+from django.db import models
+
+# Create your models here.
+class TRAIN(models.Model):
     Train_ID = models.CharField(primary_key=True, max_length=6, default="000001")
-    Model = models.CharField(primary_key=False, max_length=4, default="0001")
-    Max_Speed = models.PositiveIntegerField(default=0)
-    No_of_Seats = models.PositiveIntegerField(default=0)
-    No_of_Toilets = models.PositiveIntegerField(default=0)
-    Reclining_Seats = models.BooleanField(default=False)
-    Folding_Tables = models.BooleanField(default=False)
-    Disability_Access = models.BooleanField(default=False)
-    Luggage_Storage = models.BooleanField(default=False)
-    Vending_Machines = models.BooleanField(default=False)
-    Food_Service = models.BooleanField(default=False)
-    train_model_series = [
-        ("S","S"),
-        ("A","A"),
-    ]
-    Train_Type = models.CharField(primary_key=False, default="A", choices=train_model_series)
-
-class Route(models.Model):
-    Route_ID = models.CharField(primary_key=True, max_length=3, default="001")
-    train_routes = [
-        ("L","L"),
-        ("I","I"),
-    ]
-    Route_Type = models.CharField(primary_key=False, default="L", choices=train_routes)
-
-class S_Series(models.Model):
-    S_Train_ID = models.OneToOneField(
-        Train,
-        on_delete=models.CASCADE
+    Model = models.OneToOneField(
+        
     )
 
-class A_Series(models.Model):
-    A_Train_ID = models.OneToOneField(
-        Train,
-        on_delete=models.CASCADE
-    )
+class TRAIN_MODEL(models.Model):
+    pass
 
-class Local(models.Model):
-    L_Route_ID = models.OneToOneField(
-        Route,
-        on_delete=models.CASCADE
-    )
+class ROUTE(models.Model):
+    pass
 
-class InterTown(models.Model):
-    I_Route_ID = models.OneToOneField(
-        Route,
-        on_delete=models.CASCADE
-    )
+class S_SERIES(models.Model):
+    pass
+
+class A_SERIES(models.Model):
+    pass
+
+class LOCAL(models.Model):
+    pass
+
+class INTER_TOWN(models.Model):
+    pass
+
+class S_SERIES_ROUTING(models.Model):
+    pass
+
+class A_SERIES_ROUTING(models.Model):
+    pass
