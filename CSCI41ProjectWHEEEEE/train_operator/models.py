df67da6073f3a30a88fc76d6803a4c962c9c0682
NeptuneReal <38521285+Neptuneee11@users.noreply.github.com>
2023-12-05 13:31:33 +0800

    get abs url for train operator done

--- CSCI41ProjectWHEEEEE/train_operator/models.py
+++ CSCI41ProjectWHEEEEE/train_operator/models.py
@@ -7,29 +7,35 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 # Create your models here.
-class TRAIN(models.Model):
-    Train_ID = models.CharField(primary_key=True, max_length=6, default="000001")
-    Model = models.OneToOneField(MODEL,
-        on_delete=models.CASCADE,
-    )
-        
-
 class TRAIN_MODEL(models.Model):
     Model = models.CharField(primary_key=True, max_length=10, default="S-000")
     Max_Speed = models.CharField(primary_key=False, max_length=10, default="000 kph") 
-    No._of_Seats = models.PositiveIntegerField(primary_key=False, default=0, validators=[MinValueValidator(0), MaxValueValidator(999)])
-    No._of_Toilets = models.PositiveIntegerField(primary_key=False, default=0, validators=[MinValueValidator(0), MaxValueValidator(99)])
+    No_of_Seats = models.PositiveIntegerField(primary_key=False, default=0, validators=[MinValueValidator(0), MaxValueValidator(999)])
+    No_of_Toilets = models.PositiveIntegerField(primary_key=False, default=0, validators=[MinValueValidator(0), MaxValueValidator(99)])
     Reclining_Seats = models.BooleanField(primary_key=False, default=False)
     Folding_Tables = models.BooleanField(primary_key=False, default=False)
     Disability = models.BooleanField(primary_key=False, default=False)
     Luggage_Storage = models.BooleanField(primary_key=False, default=False)
     Vending_Machines = models.BooleanField(primary_key=False,default=False)
     Food_Service = models.BooleanField(primary_key=False,default=False)
-    Train_Type = models.CharField(primary_key=False,max_length=2, default="A") 
+    Train_Type = models.CharField(primary_key=False,max_length=2, default="A")
+
+class TRAIN(models.Model):
+    Train_ID = models.CharField(primary_key=True, max_length=6, default="000001")
+    Model = models.OneToOneField(
+        TRAIN_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def get_absolute_url(self):
+        return '{}'.format(self.pk)
 
 class ROUTE(models.Model):
     Route_ID = models.CharField(primary_key=True, max_length=3, default="001")
     Train_Type = models.CharField(primary_key=False,max_length=2, default="L") 
+
+    def get_absolute_url(self):
+        return '{}'.format(self.pk)
 
 class S_SERIES(models.Model):
     S_Train_ID = models.CharField(primary_key=True, max_length=10, default="S-001")
@@ -45,20 +51,20 @@ class INTER_TOWN(models.Model):
 
 class S_SERIES_ROUTING(models.Model):
     S_Train_ID = models.ForeignKey(
-        S-SERIES,
+        S_SERIES,
         on_delete=models.CASCADE,
         ) 
-    I_Route_ID = ForeignKey(
+    I_Route_ID = models.ForeignKey(
         INTER_TOWN,
         on_delete=models.CASCADE,
         ) 
 
 class A_SERIES_ROUTING(models.Model):
     A_Train_ID = models.ForeignKey(
-        A-SERIES,
+        A_SERIES,
         on_delete=models.CASCADE,
         ) 
-    L_Route_ID = ForeignKey(
+    L_Route_ID = models.ForeignKey(
         LOCAL,
         on_delete=models.CASCADE,
         ) 
