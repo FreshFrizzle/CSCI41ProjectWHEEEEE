be70656bd4e7fb39c4499e47ec28e50b6f4a8f66
NeptuneReal <38521285+Neptuneee11@users.noreply.github.com>
2023-12-05 18:29:26 +0800

    first two screen done on paper

--- CSCI41ProjectWHEEEEE/train_maintenance/views.py
+++ CSCI41ProjectWHEEEEE/train_maintenance/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from CSCI41ProjectWHEEEEE.train_operator.models import TRAIN_MODEL
-from CSCI41ProjectWHEEEEE.train_maintenance.models import TASK, MAINTENANCE, MAINTENANCE_LOGS, MAINTENANCE_TASK
 # Create your views here.
 
 
@@ -13,14 +12,12 @@ class TrainMaintenanceTasksView(View):
     #list down the task for the given train model
     def get(self, request, maintenance_ID=None, *args, **kwargs):
 
-        
-
-        train_model = self.request.GET.get('TRAIN_MODEL')
+        #train_model and train_ID are one-to-one
+        train_model = self.request.GET.get('Model')
 
         #get tasks related to the train_model
-        #train_model and train_ID are one-to-one
-        
-        tasks = TASK.objects.all()
+        maintenance_tasks = train_model.maintenance_num_set.all()
+        tasks = maintenance_tasks.task_id.all()
 
         return render(request, 'train_maintenance/Task-Train_Model.html', {"train_model":train_model, "tasks":tasks})
 
